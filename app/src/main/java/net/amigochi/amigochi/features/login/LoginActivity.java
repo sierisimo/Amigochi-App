0b46c08bf0c6110a47a48905d25b6af57bb86cf1
Sier Violencia <sierisimo@gmail.com>
2016-06-20 22:55:37 -0500

    Login started, layout visible

--- app/src/main/java/net/amigochi/amigochi/features/login/LoginActivity.java
+++ app/src/main/java/net/amigochi/amigochi/features/login/LoginActivity.java
@@ -15,6 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        init();
+    }
+
+    private void init(){
 
     }
 }
