537efbe259aee7137eb35b159409e1aa70b891d5
Sier Violencia <sierisimo@gmail.com>
2016-06-21 02:51:54 -0500

    Working on callbacks

--- app/src/main/java/net/amigochi/amigochi/features/statusbar/OnStatusChange.java
+++ app/src/main/java/net/amigochi/amigochi/features/statusbar/OnStatusChange.java
@@ -4,4 +4,11 @@ package net.amigochi.amigochi.features.statusbar;
  * Created by sierisimo on 21/06/16.
  */
 public interface OnStatusChange {
+    void onPointsAdded(int pointsToAdd);
+
+    void onPointsReduced(int pointsToReduce);
+
+    void onMoneyAdded(double moneyToAdd);
+
+    void onMoneyRemoved(double moneToRemove);
 }
--- app/src/main/java/net/amigochi/amigochi/features/statusbar/StatusBarFragment.java
+++ app/src/main/java/net/amigochi/amigochi/features/statusbar/StatusBarFragment.java
@@ -2,10 +2,12 @@ package net.amigochi.amigochi.features.statusbar;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import net.amigochi.amigochi.R;
 
@@ -13,9 +15,14 @@ import net.amigochi.amigochi.R;
  * Created by sierisimo on 21/06/16.
  */
 public class StatusBarFragment extends Fragment implements OnStatusChange {
+    private int points;
+
+    private double money;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
     }
 
     @Nullable
@@ -24,7 +31,36 @@ public class StatusBarFragment extends Fragment implements OnStatusChange {
         View view = inflater.inflate(R.layout.fragment_statusbar, container, false);
 
 
-
         return view;
+    }
+
+    private void setPointsText(String value) {
+        if (getView() != null) {
+            ((TextView) getView().findViewById(R.id.tv_fr_statusbar_points)).setText(value);
+        }
+    }
+
+    @Override
+    public void onPointsAdded(int pointsToAdd) {
+        points += pointsToAdd;
+
+        setPointsText(String.format("%d", points));
+    }
+
+    @Override
+    public void onPointsReduced(int pointsToReduce) {
+        points -= pointsToReduce;
+
+        setPointsText(String.format(String.format("%d",points)));
+    }
+
+    @Override
+    public void onMoneyAdded(double moneyToAdd) {
+
+    }
+
+    @Override
+    public void onMoneyRemoved(double moneToRemove) {
+
     }
 }
