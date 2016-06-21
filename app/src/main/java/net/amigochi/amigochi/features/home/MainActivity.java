af21af606a9d6efad8983d3e1542a51103d84e77
Sier Violencia <sierisimo@gmail.com>
2016-06-21 01:33:43 -0500

    Amigochi Icon added, also finished the menu

--- app/src/main/java/net/amigochi/amigochi/features/home/MainActivity.java
+++ app/src/main/java/net/amigochi/amigochi/features/home/MainActivity.java
@@ -2,9 +2,13 @@ package net.amigochi.amigochi.features.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import net.amigochi.amigochi.R;
 
@@ -12,12 +16,24 @@ import net.amigochi.amigochi.R;
  * Created by sierisimo on 20/06/16.
  */
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setupToolbar();
+
+        mDrawer = (DrawerLayout) findViewById(R.id.dl_ac_home);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nv_ac_home);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                return onOptionsItemSelected(item);
+            }
+        });
     }
 
     private void setupToolbar() {
@@ -34,5 +50,25 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.vector_menu);
             actionBar.setTitle(R.string.title_activity_home);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_amigochi:
+
+                break;
+            case R.id.action_pelota:
+
+                break;
+        }
+
+        item.setChecked(true);
+        mDrawer.closeDrawers();
+
+        return true;
     }
 }
